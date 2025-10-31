ac397bd62fc006c218dc74850ee12da7c3bb5e4a
damu <170484560+damusama@users.noreply.github.com>
2025-10-31 17:01:33 +0900

    Delete portpho directory

--- portpho/h.js
+++
@@ -1,8 +0,0 @@
-// ページ全体の読み込みが完了したら実行
-window.addEventListener('load', function() {
-    // ローディング画面の要素を取得
-    const loadingScreen = document.getElementById('loading');
-    
-    // loadingScreenに 'loaded' クラスを追加して非表示にする
-    loadingScreen.classList.add('loaded');
-});
